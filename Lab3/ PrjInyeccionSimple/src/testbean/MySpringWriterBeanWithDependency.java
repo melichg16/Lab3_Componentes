8383617b781d0b3a9f54577cc7d3a95a8fec7373
Melissa <71845479+melichg16@users.noreply.github.com>
2022-02-23 15:42:03 -0600

    Add files via upload

---
+++ Lab3/ PrjInyeccionSimple/src/testbean/MySpringWriterBeanWithDependency.java
@@ -0,0 +1,14 @@
+package testbean;
+import org.springframework.beans.factory.annotation.Autowired;
+import writer.Writer;
+public class MySpringWriterBeanWithDependency {
+    private Writer writer;
+    @Autowired
+    public void setWriter (Writer pWriter){
+        this.writer = pWriter;
+    }
+    public void run(){
+        String s = "Sample string";
+        writer.write(s);
+    }
+}
